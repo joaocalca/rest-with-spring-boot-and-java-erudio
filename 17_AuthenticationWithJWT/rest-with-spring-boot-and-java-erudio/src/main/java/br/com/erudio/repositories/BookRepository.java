3506985bb26f9d88ff0737750db4d1f0ce59c874
Joao Victor <desenvolvimento.joaocalca@gmail.com>
2023-02-15 10:58:14 -0300

    Implementing Authentication With JWT

---
+++ 17_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/repositories/BookRepository.java
@@ -0,0 +1,8 @@
+package br.com.erudio.repositories;
+
+import br.com.erudio.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {}
